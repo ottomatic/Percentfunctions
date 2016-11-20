f115e2f57f7ced43404e727b7fa388640bd42821
Otto Dandenell <otto@fetaste.com>
2016-11-20 23:36:29 +0100

    Initial skeleton of solution with test project.

---
+++ Percentfunctions/Percentfunctions.Test/PercentfunctionsTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Percentfunctions.Test
+{
+    [TestFixture]
+    public class PercentfunctionsTests
+    {
+        [TestCase]
+        public void FootTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Percentfunctions/src/Percentfunctions/Percentfuntions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Percentfunctions
+{
+    public class Percentfuntions
+    {
+        public Percentfuntions()
+        {
+        }
+
+        public static int Percentrank(int? elementValue, int?[]elements)
+        {
+            return 50;
+        }
+    }
+}
